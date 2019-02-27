f3037fe80a101a954644818e7e96d377a80db09e
Uhiriwe <u.audace@gmail.com>
2019-02-27 21:52:06 +0200

    fixing deployment issues#1

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -13,11 +13,11 @@ export class AuthService {
   private userId: string;
 
   constructor(private http: HttpClient,
-              private router: Router) {}
+    private router: Router) { }
 
   createUser(email: string, password: string) {
     return new Promise((resolve, reject) => {
-      this.http.post('api/auth/signup', {email: email, password: password}).subscribe(
+      this.http.post('/api/auth/signup', { email: email, password: password }).subscribe(
         (response: { message: string }) => {
           resolve(response);
         },
@@ -38,8 +38,8 @@ export class AuthService {
 
   loginUser(email: string, password) {
     return new Promise((resolve, reject) => {
-      this.http.post('/api/auth/login', {email: email, password: password}).subscribe(
-        (response: {userId: string, token: string}) => {
+      this.http.post('//api/auth/login', { email: email, password: password }).subscribe(
+        (response: { userId: string, token: string }) => {
           this.userId = response.userId;
           this.authToken = response.token;
           this.isAuth$.next(true);
